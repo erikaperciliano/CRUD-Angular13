c4a5fbfac795e7b1e8e7fc6c0e9a94cafc6383a9
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-01-28 15:57:35 -0300

    calling NavComponent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,18 +8,26 @@ import { HeaderComponent } from './component/template/header/header.component';
 import { FooterComponent } from './component/template/footer/footer.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavComponent } from './component/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
