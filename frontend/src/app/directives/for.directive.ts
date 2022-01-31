26a175f9bb5456eaa0ebfad06a898944e300a833
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-01-31 14:19:43 -0300

    creating structural directive with implicit values

--- frontend/src/app/directives/for.directive.ts
+++ frontend/src/app/directives/for.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[myFor]'
@@ -6,14 +6,19 @@ import { Directive, Input, OnInit } from '@angular/core';
 export class ForDirective implements OnInit {
 
   @Input('myForIn') numbers!: number[];
-  @Input('myForUsing') text!: string;
 
 
-  constructor() {
-    console.log('MyFor')
+  constructor(
+    private container: ViewContainerRef, 
+    private template: TemplateRef<any>
+  ) {
   }
   ngOnInit(): void {
-    console.log(this.numbers)
-    console.log(this.text)
+    for(let number of this.numbers) {
+      this.container.createEmbeddedView(
+        this.template, 
+        { $implicit: number}
+      )
+    }
   }
 }
