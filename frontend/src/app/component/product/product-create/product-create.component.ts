0827c1127e6503323dbb38fc2aa4d9a40378d88c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-02-01 11:48:42 -0300

    creating new product
    set value of product manually

--- frontend/src/app/component/product/product-create/product-create.component.ts
+++ frontend/src/app/component/product/product-create/product-create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService  } from '../product.service';
 import { Router } from '@angular/router';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
@@ -9,13 +10,22 @@ import { Router } from '@angular/router';
 })
 export class ProductCreateComponent implements OnInit {
 
+  product: Product = {
+    name:'Product Test',
+    price: 125.98
+  }
+
   constructor(private productService: ProductService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   createProduct(): void {
-    this.productService.showMessage('Product created successfully!');
+    //after request, it´ll show this message and return to router of products
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Product created successfully!');
+      this.router.navigate(['/products']);
+    })
   }
 
   cancel(): void {
