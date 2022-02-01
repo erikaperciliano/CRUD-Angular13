78001a15bf4b6d9dee1295618c2559143956e25c
Erika Perciliano <erikaperciliano.developer@gmail.com>
2022-02-01 18:01:23 -0300

    importing ProductReadComponent

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,6 +26,7 @@ import  { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './component/product/product-read/product-read.component';
 
 
 
@@ -39,7 +40,8 @@ import { MatInputModule } from '@angular/material/input';
     ProductCrudComponent,
     ProductCreateComponent,
     RedDirective,
-    ForDirective
+    ForDirective,
+    ProductReadComponent
   ],
   imports: [
     BrowserModule,
